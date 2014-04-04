dbe9f4ce5e7fc19b2c000d4ea1258df2e0882f83
RonnieSan <ronnie.garcia@thecontrolgroup.com>
2014-04-04 09:27:31 -0700

    Fixed version number and removed console.log

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
 var through = require('through2'),
 	gutil   = require('gulp-util'),
-	path    = require('path'),
 	fs      = require('fs'),
-	url     = require('url'),
-	imports = [];
+	path    = require('path'),
+	imports = [],
+	appRoot = path.dirname(module.parent.filename);
 
 const PLUGIN_NAME = 'gulp-importjs';
 
-function importJSStream(content) {
+function importStream(content) {
 	var stream = through();
-	stream.write(replaceImports(content));
+	stream.write(content);
 	return stream;
 }
 
 function importJS() {
 
 	var stream = through.obj(function(file, enc, callback) {
-		
+
 		if (file.isNull()) {
 			// Do nothing
 		}
 		
-		if (file.isBuffer() && content.match(/@import[^;|\n]*(;|\n)/gi) !== null) {
-			file.contents = replaceImports(file.contents);
+		if (file.isBuffer()) {
+			file.contents = new Buffer(replaceImports(file.contents.toString()));
 		}
 
 		if (file.isStream()) {
-			file.contents = file.contents.pipe(importJSStream(file.contents));
+			file.contents = file.contents.pipe(importStream(new Buffer(replaceImports(file.contents.toString()))));
 		}
 
 		this.push(file);
 		return callback();
-	})
+	});
 
 	return stream;
 }
 
 function replaceImports(content) {
-	var matches = content.match(/@import[^;|\n]*(;|\n)/gi);
-	var limit   = matches.length;
+	var matches = content.match(/@import.+;?/gi);
 
 	if (matches !== null) {
+		var limit = matches.length;
 		for (var n = 0; n < limit; n++) {
 			match    = matches[n];
-			fileName = match.replace(/@import|[\'\"\s\;\n']/gi); // Extract the filepath
-			fs.existsSync(fileName, function(fileExists) {
+			fileName = appRoot + match.replace(/@import|[\'\"\s\;\n']/gi, ""); // Extract the filepath
 
-				// The file exists, add it to an imported array and include it in the content
-				if (fileExists) {
+			console.log(fileName);
+
+			if (fs.existsSync(fileName)) {
+
+				// If the file was already imported somewhere, don't import it again
+				if (imports.indexOf(fileName) === -1) {
 					imports.push(fileName);
 					importContents = fs.readFileSync(fileName);
 					content = content.replace(match, importContents + "\n");
+					content = replaceImports(content);
 				} else {
-					content = content.replace(match, "// Error importing: " + match);
+					content = content.replace(match, "// Already included: " + match + "\n");
 				}
-
-			});
+			} else {
+				content = content.replace(match, "// Error importing: " + match + "\n");
+			}
 		}
-
-		replaceImports(content);
 	}
 
 	return content;
